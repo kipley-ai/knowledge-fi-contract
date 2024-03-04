994daf8f21aace4c3e3b5175219d472add584fc4
liaoyuan <bizliaoyuan@gmail.com>
2024-03-04 23:38:54 +0800

    v2

--- contracts/KFI.sol
+++ contracts/KFI.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KnowledgeFiToken is ERC20, Ownable {
+
+    bool stopMint = false;
+
     constructor(address initialOwner)
         ERC20("KnowledgeFi Token", "KFI")
         Ownable(initialOwner)
@@ -12,7 +16,15 @@ contract KnowledgeFiToken is ERC20, Ownable {
         _mint(msg.sender, 100000000000000000000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public{
+        require(amount<=1000 * 10 ** decimals(), "Amount too big");
+        require(balanceOf(to)<=100000 * 10 ** decimals(), "Your balance too big");
+        require(!stopMint, "Can't Mint");
         _mint(to, amount);
     }
+
+    function setMintStatus(bool status) public onlyOwner {
+        stopMint = status;
+    }
+
 }
